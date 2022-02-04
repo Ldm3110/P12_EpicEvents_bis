a660b7ef90344af3ec6789f97756d6a31a892e74
Ldm3110 <ludovic.dematteis@icloud.com>
2022-02-04 13:32:33 +0100

    Init admin interface for Events

--- EpicEvents/api_events/admin.py
+++ EpicEvents/api_events/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from api_events.models import Events
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'support_contact', 'client', 'contract', 'attendees', 'event_date', 'status',)
+    ordering = ('id',)
+    list_filter = ('client', 'sales_contact', 'support_contact')
+
+
+admin.site.register(Events, EventAdmin)
