7b4973b78dda92f6cc3b27dbed6fbd56e3f0feaf
Ldm3110 <ludovic.dematteis@icloud.com>
2022-02-04 10:44:19 +0100

    Creates Employees model and Assignment model for each of them

---
+++ EpicEvents/api_users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+roles = (
+    ('Sales', 'Sales Team'),
+    ('Support', 'Support Team')
+)
+
+
+class Employees(AbstractUser):
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employees'
+
+    def __str__(self):
+        return f"{self.username}"
+
+    def __repr__(self):
+        return f"#{self.id} - {self.username}"
+
+
+class Assignment(models.Model):
+    employee = models.OneToOneField(Employees, on_delete=models.CASCADE)
+    department = models.CharField(max_length=20, choices=roles)
