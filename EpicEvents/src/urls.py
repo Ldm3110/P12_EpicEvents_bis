ebb7614e361fcf038bf89e7ec5fcd3afcb6423fe
Ldm3110 <ludovic.dematteis@icloud.com>
2022-02-04 13:34:29 +0100

    create all events uri

--- EpicEvents/src/urls.py
+++ EpicEvents/src/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from api_contracts.views import ContractListAll, ContractCreation, ContractListFilter, ContractDetail
 from api_customers.views import CustomerList, CustomerCreation, CustomerFilterList, CustomerDetail
+from api_events.views import EventListAll, EventCreation, EventListFilter, EventDetail, CustomerEventList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,4 +20,11 @@ urlpatterns = [
     path('api/create-contract/', ContractCreation.as_view(), name='contract-creation'),
     path('api/contracts', ContractListFilter.as_view(), name='contract-filter-list'),
     path('api/contracts/<int:pk>/', ContractDetail.as_view(), name='contract-update-destroy'),
+    # URI CONTRACTS
+    path('api/events/', EventListAll.as_view(), name='events-list'),
+    path('api/create-event/', EventCreation.as_view(), name='event-creation'),
+    path('api/events', EventListFilter.as_view(), name='event-filter-list'),
+    path('api/events/<int:pk>/', EventDetail.as_view(), name='event-update-destroy'),
+    path('api/event-customer', CustomerEventList.as_view(), name='customer-of-event')
+
 ]
